9c0b0b33609ccf8c6b7d12432940ca954aee547f
Mario Garcia <mario.ggar@gmail.com>
2023-02-09 01:45:13 +0100

    feat: tablesaw groovy extensions

---
+++ modules/matter-tablesaw/src/main/groovy/com/github/grooviter/matter/tablesaw/TablesawExtensions.groovy
@@ -0,0 +1,108 @@
+package com.github.grooviter.matter.tablesaw
+
+import tech.tablesaw.api.BooleanColumn
+import tech.tablesaw.api.ColumnType
+import tech.tablesaw.api.DateColumn
+import tech.tablesaw.api.DateTimeColumn
+import tech.tablesaw.api.DoubleColumn
+import tech.tablesaw.api.FloatColumn
+import tech.tablesaw.api.InstantColumn
+import tech.tablesaw.api.IntColumn
+import tech.tablesaw.api.LongColumn
+import tech.tablesaw.api.ShortColumn
+import tech.tablesaw.api.StringColumn
+import tech.tablesaw.api.Table
+import tech.tablesaw.api.TextColumn
+import tech.tablesaw.api.TimeColumn
+import tech.tablesaw.columns.Column
+
+/**
+ * Groovy extensions for Tablesaw (https://github.com/jtablesaw/tablesaw)
+ *
+ * @since 1.0.0
+ */
+class TablesawExtensions {
+    static Table iloc(Table source, IntRange indexes, List cols) {
+        return byIntRangeAndColumnList(source, indexes, cols)
+    }
+
+    static Table iloc(Table source, IntRange indexRange, String column) {
+        return byIndexRangeAndStringColumn(source, indexRange, column)
+    }
+
+    static Table iloc(Table source, IntRange indexRange, Integer column) {
+        return byIndexRangeAndIntegerColumn(source, indexRange, column)
+    }
+
+    static Column<?> getAt(Table source, String columnName) {
+        return source.column(columnName)
+    }
+
+    static Table getAt(Table source, IntRange indexRange) {
+        return source.rows(indexRange.toList() as int[])
+    }
+
+    static Table getAt(Table source, IntRange indexRange, List cols) {
+        return byIntRangeAndColumnList(source, indexRange, cols)
+    }
+
+    static Table getAt(Table source, IntRange indexRange, IntRange columnIndex) {
+        List<Column<?>> columns = source.columns(columnIndex.toList() as int[])
+        return source.rows(indexRange.toList() as int[]).select(columns*.name() as String[])
+    }
+
+    static Table getAt(Table source, IntRange indexRange, String column) {
+        return byIndexRangeAndStringColumn(source, indexRange, column)
+    }
+
+    static Table getAt(Table source, IntRange indexRange, Integer column) {
+        return byIndexRangeAndIntegerColumn(source, indexRange, column)
+    }
+
+    static <U> Table putAt(Table source, String key, Column<U> replaceBy) {
+        return source.addColumns(resolveColumn(key, replaceBy).append(replaceBy))
+    }
+
+    private static Table byIndexRangeAndStringColumn(Table source, IntRange indexRange, String column) {
+        return source.rows(indexRange.toList() as int[]).select(column)
+    }
+
+    private static byIndexRangeAndIntegerColumn(Table source, IntRange indexRange, Integer column) {
+        return source.rows(indexRange.toList() as int[]).select(source.column(column).name())
+    }
+
+    private static Table byIntRangeAndColumnList(Table source, IntRange indexRange, List cols) {
+        boolean areNumbers = cols.every { it instanceof Integer }
+        boolean areNames = cols.every { it instanceof String }
+
+        if (!(areNumbers || areNames)) {
+            throw new RuntimeException("cols should either column names or index numbers")
+        }
+
+        if (areNumbers) {
+            Column<?>[] columns = source.columns(cols as int[])
+            return source.rows(indexRange.toList() as int[]).select(columns*.name() as String[])
+        }
+
+        return source.rows(indexRange.toList() as int[]).select(cols*.toString() as String[])
+    }
+
+    private static <T> Column<T> resolveColumn(String name, Column<T> source) {
+        switch(source.type()) {
+            case ColumnType.STRING: return StringColumn.create(name)
+            case ColumnType.INTEGER: return IntColumn.create(name)
+            case ColumnType.DOUBLE: return DoubleColumn.create(name)
+            case ColumnType.BOOLEAN: return BooleanColumn.create(name)
+            case ColumnType.FLOAT: return FloatColumn.create(name)
+            case ColumnType.INSTANT: return InstantColumn.create(name)
+            case ColumnType.LOCAL_DATE: return DateColumn.create(name)
+            case ColumnType.LOCAL_DATE_TIME: return DateTimeColumn.create(name)
+            case ColumnType.LOCAL_TIME: return TimeColumn.create(name)
+            case ColumnType.LONG: return LongColumn.create(name)
+            case ColumnType.SHORT: return ShortColumn.create(name)
+            case ColumnType.TEXT: return TextColumn.create(name)
+            default:
+                return StringColumn.create(name)
+        }
+    }
+}
